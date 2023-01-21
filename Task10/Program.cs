acddd3f541e9e670a79ec806206480feb8b6222f
Yervand <hakobyan.ea@gmail.com>
2023-01-22 01:47:37 +0300

    Exequte Task 10

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -8,3 +8,21 @@
 // 782 -> 8
 // 918 -> 1
 
+Console.Write("Введите трёхзначное число: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+bool threeDigit = ThreeDigit(number);
+
+int secondDigit = number / 10 % 10;
+
+bool ThreeDigit(int num)
+{
+   return num > 99 && num < 1000;
+}
+Console.WriteLine(threeDigit ? $"{secondDigit}": "Введено некорректное число");
+
+
+
+
+
