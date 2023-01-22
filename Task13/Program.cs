9288b460e4c7c2a9d551e2434411e55acb6d8c25
Yervand <hakobyan.ea@gmail.com>
2023-01-22 04:47:35 +0300

    Exequte Task 13

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -9,3 +9,35 @@
 // 78 -> третьей цифры нет
 
 // 32679 -> 6
+
+Console.Write("Введите число: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+int thirdNum = ThirdNum(number);
+
+int ThirdNum(int num)
+{
+    if(num > 99)
+    {
+        while(num > 999)
+        {
+            num = num / 10;
+        }
+        if(num < 1000)
+        {
+            num = num % 10;
+            
+            Console.WriteLine($"{num}");
+        }       
+           
+    }
+    else
+    {
+        Console.WriteLine("Третьей цифры нет");
+    }
+        return num;
+    
+}
+
+
