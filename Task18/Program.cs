7236543f0e57a9144587967cf63d6eace07a40b0
Yervand <hakobyan.ea@gmail.com>
2023-01-23 05:28:05 +0300

    add Task 18/20

---
+++ Task18/Program.cs
@@ -0,0 +1,3 @@
+﻿// double d = 5.099753443;
+// double dRound = Math.Round(d, 2, MidpointRounding.ToZero);
+// Console.WriteLine(dRound);
---
+++ Task20/Program.cs
@@ -0,0 +1,31 @@
+﻿//  Напишите программу, которая
+// принимает на вход координаты двух точек и
+// находит расстояние между ними в 2D
+// пространстве.
+// A (3,6); B (2,1) -> 5,09
+// A (7,-5); B (1,-1) -> 7,21
+
+
+
+
+
+
+Console.Write("Введите координаты точки x1 -> ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координаты точки y1 -> ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координаты точки x2 -> ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координаты точки y2 -> ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+double distance = Distance(x1, y1, x2, y2);
+double Distance(int xa, int ya, int xb, int yb)
+{
+    int kat1 = xa - xb;
+    int kat2 = ya - yb;
+    double result = Math.Sqrt(kat1 * kat1 + kat2 * kat2);
+    return result;
+}
+
+double distanceRound = Math.Round(distance, 2, MidpointRounding.ToZero);
+Console.WriteLine($"Расстояние = { distanceRound}");
