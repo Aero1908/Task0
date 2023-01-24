a15fbd1ddc6cc528217f3281912333e610dde74f
Yervand <hakobyan.ea@gmail.com>
2023-01-24 22:19:18 +0300

    add Task26/28/30

---
+++ Task26/Program.cs
@@ -0,0 +1,45 @@
+﻿// Напишите программу, которая принимает
+// на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+
+
+// Console.WriteLine("Введите целое число: ");
+// int number;
+// if(int.TryParse(Console.ReadLine(), out number))
+//     Console.WriteLine($"Количество цифр в числе -> {number} = {CountNumber(num)}");
+// else Console.WriteLine("Введены некорректные данные");
+
+// int count = 1;
+// int countNumber = CountNumber(number);
+
+// int CountNumber(int num)
+// {
+
+//     while (number > 0 || number < 0)
+//     {
+//         number = number / 10;
+//         count++;
+//     }
+//     return count;
+// }
+
+int DigitsNumber(int num)
+{
+    int count = 0;
+    while ((int)Math.Abs(num) > 0)
+    {
+        num = num / 10;
+        count++;
+    }
+    return (count);
+}
+
+Console.Write("Введите целое число: ");
+int number;
+if (int.TryParse(Console.ReadLine(), out number))
+    Console.WriteLine($"Количество цифр в числе {number} = {DigitsNumber(number)}");
+else
+    Console.WriteLine("Введено некорректное значение. Введите целое число");
---
+++ Task30/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая
+// выводит массив из 8 элементов, заполненный
+// нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+void MassiveFiller(int[] col)
+{
+    for (int i = 0; i < col.Length; i++)
+        col[i] = new Random().Next(0, 2);
+}
+
+
+
+void MassiveСonclusion(int[] col) 
+{
+    Console.Write("Получившийся  массив: ");
+    for (int i = 0; i < col.Length; i++)
+    {
+        Console.Write($"  {col[i]}");
+    }
+}
+
+int[] array = new int[8];
+MassiveFiller(array);
+MassiveСonclusion(array);
